1c163470507a7f5f7ca8fdd5c1cc0fd12f1d126d
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-09-22 21:39:16 +0530

    Time: 16 ms (89.15%), Space: 56.9 MB (29.36%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        for(int i=0; i<len; i++){
+            if(map.containsKey(nums[i])){
+                if(i-map.get(nums[i]) <= k) return true;
+                map.put(nums[i], i);
+            }
+            else map.put(nums[i], i);
+        }
+        return false;
+    }
+}
