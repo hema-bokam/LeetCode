dc9ff9e060e679dd80e7a960098254c7f028103a
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-09-29 10:23:46 +0530

    Time: 4 ms (84.7%), Space: 56.9 MB (34.21%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,17 +1,16 @@
 class Solution {
+    //cycle detect apparoach
     public int findDuplicate(int[] nums) {
-        // binary search on possible answers
-        // pigeonhole principle n+1 integers places in array of length n where elements lie between [1,n], then there must be atleast one integer repeated
-        int len = nums.length, s = 1, e = len-1;  // min and max possible values of array element
-        while(s < e){
-            int mid = (s+e)/2;
-            int count = 0;
-            for(int i=0; i<len; i++){
-                if(nums[i] <= mid) count++;
-            }
-            if(count <= mid) s = mid+1; 
-            else e = mid;  // count>mid which means repeated number will be there. mid is also possible answer
+        int slow = 0, fast = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        int p1 = 0, p2 = fast;
+        while(p1 != p2){
+            p1 = nums[p1];
+            p2 = nums[p2];
         }
-        return s;
+        return p1;
     }
 }
