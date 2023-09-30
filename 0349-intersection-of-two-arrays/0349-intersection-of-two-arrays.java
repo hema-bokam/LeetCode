17a48cfcc0490b3bd6ce6dfdf4132e949e18b431
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-09-30 17:12:56 +0530

    Time: 5 ms (21.47%), Space: 42.5 MB (77.7%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
+        int len1 = nums1.length;
+        HashSet<Integer> set = new HashSet<>();
         Arrays.sort(nums2);
-        List<Integer> arr = new ArrayList<>();
-        int i=0, j=0, len1 = nums1.length, len2 = nums2.length;
-        while(i<len1 && j<len2){
-            if(nums1[i] < nums2[j]) i++;
-            else if(nums2[j] < nums1[i]) j++;
-            else{
-                int val = nums1[i];
-                arr.add(val);
-                while(i<len1 && nums1[i] == val) i++;
-                while(j<len2 && nums2[j] == val) j++;
-            }
+        for(int val : nums1){
+            if(binarySearch(nums2, val)) set.add(val);
         }
-        int[] res = arr.stream().mapToInt(val -> val).toArray();
-        return res;
+        return set.stream().mapToInt(val -> val).toArray();
+    }
+    public boolean binarySearch(int[] nums, int target){
+        int s=0, e=nums.length-1;
+        while(s <= e){
+            int m = s + (e-s)/2;
+            if(nums[m] == target) return true;
+            else if(nums[m] < target) s = m+1;
+            else e = m-1;
+        }
+        return false;
     }
 }
