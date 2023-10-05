284a77fe52b5c284706ca34db0e40d917f253209
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-05 13:58:01 +0530

    Time: 0 ms (100%), Space: 41.2 MB (40.71%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int len = nums.length, low = 0, high = len-1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] < nums[mid+1]) low = mid+1;
+            else high = mid;
+        }
+        return low;
+    }
+}
