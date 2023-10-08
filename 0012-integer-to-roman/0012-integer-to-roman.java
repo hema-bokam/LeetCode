7fd114fac8a5154f5141b4a9f55beac37d0b2945
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-08 16:13:33 +0530

    Time: 13 ms (20.5%), Space: 44 MB (9.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,28 +1,9 @@
 class Solution {
     public String intToRoman(int num) {
-        int sum = 0;
-        StringBuilder sb = new StringBuilder();
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
-        for(int i=num; i>=1; i--){
-            if(map.containsKey(i)){
-                sb.append(map.get(i));
-                i = num - i + 1;
-                num = i-1;
-            }
-        }
-        return sb.toString();
+        String[] thousands = {"", "M", "MM", "MMM"};  // max possible num is 3999 
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] singleDigits = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[((num % 1000)%100) / 10] + singleDigits[((num % 1000)%100)%10];
     }
 }
