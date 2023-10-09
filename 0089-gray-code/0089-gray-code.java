30cdf306bcf6f9d48c9c1ca85613cc13797a66d5
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-09 22:43:04 +0530

    Time: 6 ms (68.94%), Space: 48.3 MB (92.5%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,10 +1,16 @@
 class Solution {
+    // 00
+    // 01
+    // 11
+    // 10
     public List<Integer> grayCode(int n) {
-        int size = 1 << n;
-        List<Integer> res = new ArrayList<>();
-        for(int i=0; i<size; i++){
-            res.add(i ^ (i >> 1));
+        if(n == 0) return new ArrayList<>(List.of(0));
+        List<Integer> prevHalf = grayCode(n-1);
+        List<Integer> currHalf = new ArrayList<>(prevHalf);
+        int setBitVal = 1 << (n-1);
+        for(int i = prevHalf.size()-1; i>=0; i--){
+            currHalf.add(prevHalf.get(i) + setBitVal);
         }
-        return res;
+        return currHalf;
     }
 }
