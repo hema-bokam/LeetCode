988d5f4dd62f934eff32b2dc7f6e2be12b68485d
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-11 16:47:06 +0530

    Time: 10 ms (84.47%), Space: 54.5 MB (92.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int len = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<len; i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
