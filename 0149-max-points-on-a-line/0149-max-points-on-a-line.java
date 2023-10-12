415ba4ca7b947801bc7562965fe2e17d9d48e333
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-12 22:18:02 +0530

    Time: 36 ms (28.27%), Space: 43.5 MB (55.19%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,34 @@
+class Solution {
+    // points that lie on same line will have same slope, slope = (y2-y1)/(x2-x1)
+    // slope can have float values, to avoid precision errors we should take denominator and numerator seperately
+    // slope <(y2-y1)/gcd , (x2-x1)/gcd> where gcd = gcd(y2-y1, x2-x1)
+    public int maxPoints(int[][] points) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int finalMax = 0;
+        int len = points.length;
+        for(int i=0; i<len; i++){
+            int samePoints = 0, maxPoints = 0;
+            for(int j=i+1; j<len; j++){
+                int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
+                // to handle 0/0 case, which means point1 and point2 are same
+                if(x1 == x2 && y1 == y2){
+                    samePoints++;
+                    continue;
+                }else{
+                    int gcd = findGcd(y2-y1, x2-x1);
+                    int numerator = (y2-y1)/gcd, denominator = (x2-x1)/gcd;
+                    String slope = numerator + "*" + denominator;
+                    map.put(slope, map.getOrDefault(slope, 0)+1);
+                    maxPoints = Math.max(maxPoints, map.get(slope)); 
+                }
+            }
+            finalMax = Math.max(finalMax, 1+samePoints+maxPoints);
+            map.clear();
+        }
+        return finalMax;
+    }
+    public int findGcd(int a, int b){
+        if(a == 0) return b;
+        return findGcd(b % a, a);
+    }
+}
