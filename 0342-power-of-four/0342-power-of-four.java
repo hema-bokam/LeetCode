c95b4d00ea25426bbf14fd32951fba01162b8e3c
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-23 15:41:44 +0530

    Time: 1 ms (76.31%), Space: 39.3 MB (72.83%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
-        while(n != 1 && n%4 == 0){
-            n /= 4;
-        }
-        return n==1 ? true : false;
+        if(n == 1) return true;
+        double logNtoBase4 = Math.log(n)/Math.log(4);
+        return logNtoBase4 == (int)logNtoBase4;
     }
 }
