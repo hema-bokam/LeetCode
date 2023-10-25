f5732a9f3a989bd0418b23ea3275ca37bef1b45e
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-25 10:58:52 +0530

    Time: 0 ms (100%), Space: 39.6 MB (9.89%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
@@ -1,8 +1,8 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-        if(k == 1) return 0;
+        if(n==1 && k==1) return 0;
         int totalEle = (int)Math.pow(2, n-1);
         if(k <= totalEle/2) return kthGrammar(n-1, k);
-        return 1^kthGrammar(n-1, k-totalEle/2);
+        return 1-kthGrammar(n-1, k-totalEle/2);
     }
 }
