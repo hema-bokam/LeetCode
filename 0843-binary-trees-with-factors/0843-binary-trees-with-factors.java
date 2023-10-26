ff1e28ad081669e06cbe6fd4b7dc83f03954edd0
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-26 13:43:20 +0530

    Time: 69 ms (9.63%), Space: 43.4 MB (47.41%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.java
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.java
@@ -1,11 +1,11 @@
-class Solution {
-    //1. HashMap to store key - rootEle and value as - number of binary trees with rootEle as root 
-    //2. Traverse array
+//1. HashMap to store key - rootEle and value as - number of binary trees with rootEle as root 
+//2. Traverse array
         // 2.1. every element itself is a binary tree with no children, so count = 1
         // 2.2. traverse hashmap and if arr[i] % key == 0 it is divisor and checkfor second divisor arr[i]/k
             //key and arr[i]/key can be left child and right child of arr[i].
         // 2.3. if arr[i]/k is present in hashmap then count += map.get(key) * map.get(arr[i]/key)
-    //3. sum += count; 
+//3. sum += count; 
+class Solution {
     public int numFactoredBinaryTrees(int[] arr) {
         HashMap<Integer, Long> dp = new HashMap<>();
         long res = 0, mod = 1000000007L;
@@ -22,7 +22,7 @@ class Solution {
             dp.put(arr[i], count);
         }
         for(Integer key : dp.keySet()){
-            res = (res % mod + dp.get(key) % mod)%mod;
+            res = (res % mod + dp.get(key) % mod) % mod;
         }
         return (int)res;
     }
