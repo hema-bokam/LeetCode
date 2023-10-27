61b1adefda5508b53ac6c328432d6d9008ae8729
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-27 13:16:55 +0530

    Time: 122 ms (33.55%), Space: 45.3 MB (18.3%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int len = s.length(), ans = 0, start = 0, end = 0;
-        for(int i=0; i<len; i++){
-            for(int j=i; j<len; j++){
-                if(isPalindrome(s.substring(i, j+1))){
-                    if(ans < j+1-i){
-                        ans = j+1-i;
-                        start = i;
-                        end = j+1;
-                    }
+        int length = s.length(), maxLen = 1, start = 0, end = 0;
+        boolean[][] dp = new boolean[length][length]; // to store wheather s[i to j] is palindrome or not
+        for(int i=0; i<length; i++){
+            //for length 1 substring
+             dp[i][i] = true;
+             //for length 2 substrings
+             if(i != length-1){
+                if(s.charAt(i) == s.charAt(i+1)){
+                    dp[i][i+1] = true;
+                    start = i;
+                    end = i+1;
+                }
+             }
+        }
+        //checking for lengths 3 to string length
+        for(int len = 3; len <= length; len++){
+            for(int i=0; i<=length-len; i++){
+                int j = i + len - 1;
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j] = true;
+                    start = i;
+                    end = j;
                 }
             }
         }
-        return s.substring(start, end);
-    }
-    public boolean isPalindrome(String s){
-        int i=0, j=s.length()-1;
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
+            
+        return s.substring(start, end+1);
     }
 }
