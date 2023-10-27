3b12224c8e5a56950260580e86cc3fbf50234c27
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-10-27 14:41:24 +0530

    Time: 17 ms (66.12%), Space: 40.9 MB (87.69%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,33 @@
 class Solution {
+    //appraoch: at every index go left and go right to check if it is palindrome or not
+    // edge cases: we need to check for both odd len and even len palindromes
     public String longestPalindrome(String s) {
-        int length = s.length(), maxLen = 1, start = 0, end = 0;
-        boolean[][] dp = new boolean[length][length]; // to store wheather s[i to j] is palindrome or not
-        for(int i=0; i<length; i++){
-            //for length 1 substring
-             dp[i][i] = true;
-             //for length 2 substrings
-             if(i != length-1){
-                if(s.charAt(i) == s.charAt(i+1)){
-                    dp[i][i+1] = true;
-                    start = i;
-                    end = i+1;
-                }
-             }
-        }
-        //checking for lengths 3 to string length
-        for(int len = 3; len <= length; len++){
-            for(int i=0; i<=length-len; i++){
-                int j = i + len - 1;
-                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
-                    dp[i][j] = true;
+        int len = s.length(), max = 1, start = 0, end = 0;
+        for(int k = 0; k<len; k++){
+            int i=k, j=k;
+            //check for odd len palindromes
+            while(i>=0 && j<len && s.charAt(i) == s.charAt(j)){
+                if(max < j-i+1){
+                    max = j-i+1;
                     start = i;
                     end = j;
                 }
+                i--;
+                j++;
+            }
+            i=k;
+            j=k+1;
+            //check for even len palindromes
+            while(i >=0 && j<len && s.charAt(i) == s.charAt(j)){
+                if(max < j-i+1){
+                    max = j-i+1;
+                    start = i;
+                    end = j;
+                }
+                i--;
+                j++;
             }
         }
-            
         return s.substring(start, end+1);
     }
 }
