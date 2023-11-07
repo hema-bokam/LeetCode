382e990f0b48a145c43e163254bffa463b119499
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-11-07 01:10:59 -0600

    Time: 64 ms (17.19%), Space: 56.4 MB (23.44%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.java
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.java
@@ -1,37 +1,15 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
-        List<Pair> arr = new ArrayList<>();
+        PriorityQueue<Double> pq = new PriorityQueue<>();
         int len = dist.length;
-        for(int i=0; i<len; i++){
-            Pair p = new Pair(dist[i], speed[i]);
-            arr.add(p);
+        for(int i=0; i<len; ++i){
+            pq.add((double)dist[i]/speed[i]);
         }
-        Collections.sort(arr, new CustomComparator());
-        int time = 0, monsters=0;
-        for(int i=0; i<len; i++){
-            Pair p = arr.get(i);
-            int distCov = time * p.speed;
-            if(p.dist <= distCov) return monsters;
-            monsters++;
-            time++;
+        int ans = 0;
+        while(!pq.isEmpty()){
+            if(pq.remove() <= ans) break;
+            ans++;
         }
-        return monsters;
-    }
-}
-class Pair{
-        int dist;
-        int speed;
-        public Pair(int dist, int speed){
-            this.dist = dist;
-            this.speed = speed;
-        }
-}
-class CustomComparator implements Comparator<Pair>{
-    @Override
-    public int compare(Pair p1, Pair p2){
-        double t1 = (double)p1.dist / (double)p1.speed;
-        double t2 = (double)p2.dist / (double)p2.speed;
-        if(t1 < t2) return -1;
-        else return (int)(t1-t2);
+        return ans;
     }
 }
