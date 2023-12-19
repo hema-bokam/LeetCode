a4589e3603a9983a32224783ae258c7c753c8157
Hema <98752136+hema-bokam@users.noreply.github.com>
2023-12-19 23:50:15 +0530

    Time: 7 ms (50.42%), Space: 45.3 MB (8.40%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (0 <= x && x < m && 0 <= y && y < n) {
+                            sum += img[x][y] & 255;
+                            count += 1;
+                        }
+                    }
+                }
+                img[i][j] |= (sum / count) << 8;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                img[i][j] >>= 8;
+            }
+        }
+        return img;
+    }
+}
