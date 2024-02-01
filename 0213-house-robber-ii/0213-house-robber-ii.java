43d51c849e24d9061a43c134b5c5e332b6383ca4
Hema <98752136+hema-bokam@users.noreply.github.com>
2024-02-02 04:25:47 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (18.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-        int[] dp = new int[len];
-        Arrays.fill(dp, -1);
-        int res1 = len == 1 ? nums[0] : findMax(dp, nums, len-2);
-        nums[0] = 0;
-        Arrays.fill(dp, -1);
-        int res2 = findMax(dp, nums, len-1);
-        return Math.max(res1, res2);
+        if(len == 1) return nums[0];
+        int[] nums1 = new int[len];
+        int[] nums2 = new int[len];
+        for(int i=0; i<len; i++){
+            if(i != 0) nums1[i] = nums[i];
+            if(i != len-1) nums2[i] = nums[i];
+        }
+        return Math.max(findMax(nums1, len), findMax(nums2, len));
     }
-    public int findMax(int[] dp, int[] nums, int n){
-        if(n < 0) return 0;
-        if(n == 0) return nums[0];
-        if(dp[n] != -1) return dp[n];
-        dp[n] = Math.max(nums[n] +  findMax(dp, nums, n-2), findMax(dp, nums, n-1));
-        return dp[n];
+    public int findMax(int[] nums, int n){
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            dp[i] = Math.max(nums[i] + (i > 1 ? dp[i-2] : 0), dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
