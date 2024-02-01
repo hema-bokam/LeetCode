6a5132880641c1cc0e64cc29158cbd0ee707b78e
Hema <98752136+hema-bokam@users.noreply.github.com>
2024-02-02 03:51:44 +0530

    Time: 0 ms (100.00%), Space: 41 MB (46.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,15 +2,10 @@ class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
         int[] dp = new int[len];
-        Arrays.fill(dp, -1);
-        return findMaxMoney(dp, nums, len-1);
-    }
-    public int findMaxMoney(int[] dp, int[] nums, int n){
-        if(n < 0) return 0;
-        if(n == 0) return dp[0] = nums[0];
-        if(dp[n] != -1) return dp[n];
-        dp[n] = Math.max(nums[n] + findMaxMoney(dp, nums, n-2),
-                        findMaxMoney(dp, nums, n-1));
-        return dp[n];
+        dp[0] = nums[0];
+        for(int i=1; i<len; i++){
+            dp[i] = Math.max(dp[i-1], (i >= 2 ? dp[i-2] : 0) + nums[i]);
+        }
+        return dp[len-1];
     }
 }
