7fa6f8c237fc8d97937d1234f5268411228f6258
Hema <98752136+hema-bokam@users.noreply.github.com>
2024-02-02 03:05:31 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (12.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return findWays(dp, n);
-    }
-    public int findWays(int[] dp, int n){
-        if(n <= 0) return 0;
-        if(n == 1) return dp[n] = 1;
-        if(n == 2) return dp[n] = 2;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = findWays(dp, n-1) + findWays(dp, n-2);
+        dp[0] = 0;
+        dp[1] = 1;
+        if(n >= 2) dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
