d8451841f7846f467e6120d4de6eff7f690a0c97
Hema <98752136+hema-bokam@users.noreply.github.com>
2024-02-10 03:30:12 +0530

    Time: 3 ms (70.03%), Space: 44.7 MB (19.03%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,24 +2,16 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int rows = triangle.size(), cols = rows, min = Integer.MAX_VALUE;
         int[][] dp = new int[rows][cols];
-        for(int i=0; i<rows; i++){
-            Arrays.fill(dp[i], -1);
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i=1; i<rows; i++){
+            for(int j=0; j<=i; j++){
+                if(j==0) dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
+                else if(j==i) dp[i][j] = triangle.get(i).get(j) + dp[i-1][j-1];
+                else dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j], dp[i-1][j-1]);
+            }
         }
-        int[] res = new int[cols];
-        for(int i=0; i<cols; i++){
-            res[i] = findMinPath(triangle, dp, rows-1, i);
-            min = Math.min(res[i], min);
-        }
+        for(int i=0; i<cols; i++) min = Math.min(dp[rows-1][i], min);
         return min;
     }
-    public int findMinPath(List<List<Integer>> tri, int[][] dp, int i, int j){
-        // if(i<0 || j<0) return ;
-        if(tri.get(i).size() <= j) return Integer.MAX_VALUE;
-        if(i==0 && j==0) return dp[i][j]=tri.get(i).get(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int path1 = i > 0 ? findMinPath(tri, dp, i-1, j) : Integer.MAX_VALUE;
-        int path2 = j > 0 ? findMinPath(tri, dp, i-1, j-1) : Integer.MAX_VALUE;
-        dp[i][j] = tri.get(i).get(j) + Math.min(path1, path2);
-        return dp[i][j];
-    }
+    
 }
