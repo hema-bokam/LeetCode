f0ae971944b91c544caf10a55e21c186499fa22b
Hema <98752136+hema-bokam@users.noreply.github.com>
2024-02-09 20:32:29 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (46.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,14 +2,14 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i=0; i<m; i++){
-            Arrays.fill(dp[i], -1);
+            for(int j=0; j<n; j++){
+                if(i == 0) dp[i][j] = 1;
+                else if(j == 0) dp[i][j] = 1;
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
         }
-        return findTotalUniquePaths(dp, m-1, n-1);
-    }
-    public int findTotalUniquePaths(int[][] dp, int m, int n){
-        if(m < 0 || n < 0) return 0;
-        if(m == 0 && n == 0) return dp[m][n] = 1;
-        if(dp[m][n] != -1) return dp[m][n];
-        return dp[m][n] = findTotalUniquePaths(dp, m-1, n) + findTotalUniquePaths(dp, m, n-1);
+        return dp[m-1][n-1];
     }
 }
